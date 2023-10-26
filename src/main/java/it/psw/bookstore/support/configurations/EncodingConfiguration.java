2dc6862f5e9b0c85a26d28bec701edd122361377
Gaetano24 <gaetano.marchiano24@gmail.com>
2023-10-26 17:31:28 +0200

    "Security Configuration implementata"

--- src/main/java/it/psw/bookstore/support/configurations/EncodingConfiguration.java
+++ src/main/java/it/psw/bookstore/support/configurations/EncodingConfiguration.java
@@ -8,13 +8,6 @@ import java.nio.charset.StandardCharsets;
 @Configuration
 public class EncodingConfiguration {
 
-    /*
-    Imposta la codifica predefinita dei messaggi JSON generati dall'applicazione su UTF-8,
-    garantendo che le risposte HTTP contenenti dati JSON siano correttamente codificate in UTF-8.
-    Tale configurazione è importante per gestire caratteri speciali e assicurare la compatibilità
-    con una vasta gamma di client e sistemi.
-     */
-
     @Bean
     public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
